258a0a3623beae6661f201ae9b959e3d84ac405c
Anuswan91 <vivien.blum@free.fr>
2017-09-13 07:34:31 -0700

    landing

---
+++ src/containers/Landing/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './styles/Landing.scss'
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing-container">
+        <p>GET STARTED</p>
+      </div>
+    )
+  }
+}
+
+export default Landing
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Route } from 'react-router'
+import { Route, IndexRoute } from 'react-router'
 import { App } from './../containers/App'
+import { Landing } from './../containers/Landing'
 
 const routes = (
     <Route path="/">
       <Route component={App}>
+        <IndexRoute component={Landing} />
       </Route>
     </Route>
 )
