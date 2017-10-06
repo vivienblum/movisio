83b1fb74b481ae52d0dbb34d16a35e9d06b39ef2
Anuswan91 <vivien.blum@free.com>
2017-10-06 15:49:03 +0200

    Footer route setup

---
+++ src/components/FooterMovisio/FooterMovisio.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from 'react-router'
+import {Footer} from "../../shared/footer";
+
+export default function FooterMovisio() {
+  // TODO set link to My movies
+  return (
+    <Footer>
+      Footer
+    </Footer>
+  )
+}
