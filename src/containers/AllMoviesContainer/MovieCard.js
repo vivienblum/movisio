c726a258f3a41341fa6a5fceeac6a1ec0d6a83dc
Anuswan91 <vivien.blum@free.fr>
2017-10-11 15:38:50 -0700

    Added icon

--- src/containers/AllMoviesContainer/MovieCard.js
+++ src/containers/AllMoviesContainer/MovieCard.js
@@ -2,9 +2,11 @@ import React, { Component } from "react"
 import { asReactiveLoader } from "mobx-models/reactiveLoader"
 import { func } from "prop-types"
 import styled from "styled-components"
+import FontIcon from "material-ui/FontIcon"
 // import FontIcon from "material-ui/FontIcon"
-// import Theme from "../../styles/Theme"
+import Theme from "../../styles/Theme"
 import { CollectionCard } from "../../shared/collection"
+import "./styles/MovieCard.scss"
 
 @asReactiveLoader
 class MovieCard extends Component {
@@ -24,12 +26,19 @@ class MovieCard extends Component {
   render() {
     const { isSelected, movie } = this.props
     return (
-      <CollectionCard
-        onClick={this.handleClick.bind(this)}
-        isSelected={isSelected}
-      >
-        <Poster alt="" src={movie.poster} />
-      </CollectionCard>
+      <div className="movie-card">
+        <CollectionCard
+          onClick={this.handleClick.bind(this)}
+          isSelected={isSelected}
+        >
+          <IconContainer className="IconContainer">
+            <FontIcon className="material-icons" color={`${Theme.lime600}`}>
+              face
+            </FontIcon>
+          </IconContainer>
+          <Poster alt="" src={movie.poster} />
+        </CollectionCard>
+      </div>
     )
   }
 }
@@ -44,3 +53,16 @@ const Poster = styled.img`
     filter: brightness(50%);
   }
 `
+
+const IconContainer = styled.div`
+  position: absolute;
+  // top: 0;
+  // left: 0;
+  padding: 4px;
+  z-index: 3;
+  flex-direction: row;
+  justify-content: space-between;
+  color: red;
+`
+
+// const IconPlatform = styled.i``
