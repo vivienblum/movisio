393abd4290e5ccb140c08582f9aeb3491f82dab9
Anuswan91 <vivien.blum@free.fr>
2017-11-04 11:39:46 -0700

    Footer with icons

--- src/components/FooterMovisio/FooterMovisio.js
+++ src/components/FooterMovisio/FooterMovisio.js
@@ -1,22 +1,31 @@
-import React from "react";
-import { Link } from 'react-router'
-import FontIcon from 'material-ui/FontIcon';
-import {Footer} from "../../shared/footer";
+import React from "react"
+import { Link } from "react-router"
+import { Footer } from "../../shared/footer"
+import FaLinkedinSquare from "react-icons/lib/fa/linkedin-square"
+import FaYoutubeSquare from "react-icons/lib/fa/youtube-square"
+import TiSocialGithubCircular from "react-icons/lib/ti/social-github-circular"
 
 export default function FooterMovisio() {
-  // TODO set link to My movies
   return (
     <Footer>
       <div>
         <h3>Movisio</h3>
-        <p>Application to manage both the movies you want to watch and the ones you have already watched.</p>
+        <p>
+          Application to manage both the movies you want to watch and the ones
+          you have already watched.
+        </p>
       </div>
       <div>
         <h3>Follow</h3>
-        <Link to="/"><FontIcon className="muidocs-icon-action-home" /></Link>
-        <Link to="https://www.linkedin.com/in/vivien-blum-944556b0/">LK</Link>
-        <a href="https://www.youtube.com/channel/UCpK7GJCqeelL29fLWzCoQNg">YT</a>
-        <a href="https://github.com/vivienblum">GT</a>
+        <Link to="https://www.linkedin.com/in/vivien-blum-944556b0/">
+          <FaLinkedinSquare />
+        </Link>
+        <a href="https://www.youtube.com/channel/UCpK7GJCqeelL29fLWzCoQNg">
+          <FaYoutubeSquare />
+        </a>
+        <a href="https://github.com/vivienblum">
+          <TiSocialGithubCircular />
+        </a>
       </div>
     </Footer>
   )
